f27267e595c5c10bd04026b4096fe7857fe22b80
mvn-datphan-dn <dat.phan@monstar-lab.com>
2022-04-19 13:42:03 +0700

    .

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -11,4 +11,10 @@
     const table = (req, res, next) => {
         res.render('admin/tables')
     }
-export { index, profile, chart, table };
+    const login = (req, res, next) => {
+        res.render('admin/signup-signin/login')
+    }
+    const register = (req, res, next) => {
+        res.render('admin/signup-signin/register')
+    }
+export { index, profile, chart, table, login, register };
--- router/admin.router.js
+++ router/admin.router.js
@@ -8,6 +8,8 @@ const initRoute = () => {
   route({ method: HttpMethod.GET, url: '/profile', action: adminController.profile, middelware: [] });
   route({ method: HttpMethod.GET, url: '/chart', action: adminController.chart, middelware: [] });
   route({ method: HttpMethod.GET, url: '/tables', action: adminController.table, middelware: [] });
+  route({ method: HttpMethod.GET, url: '/login', action: adminController.login, middelware: [] });
+  route({ method: HttpMethod.GET, url: '/register', action: adminController.register, middelware: [] });
   // route({ method: HttpMethod.POST, url: '/refresh-token', action: authController.register, middelware: [] });
 };
 
